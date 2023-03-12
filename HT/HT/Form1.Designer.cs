ee6b7406d36a92ce3e78bdf80d9faceeb158406c
Josip Krljan <126254647+storm32167@users.noreply.github.com>
2023-03-12 12:02:18 +0100

    Add files via upload

---
+++ HT/HT/Form1.Designer.cs
@@ -0,0 +1,151 @@
+﻿namespace HT
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.dtStartDate = new System.Windows.Forms.DateTimePicker();
+            this.dtEndDate = new System.Windows.Forms.DateTimePicker();
+            this.btnLoad = new System.Windows.Forms.Button();
+            this.textBoxTrackingCode = new System.Windows.Forms.TextBox();
+            this.btnSearch = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AllowUserToAddRows = false;
+            this.dataGridView1.AllowUserToDeleteRows = false;
+            this.dataGridView1.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.AllCells;
+            this.dataGridView1.AutoSizeRowsMode = System.Windows.Forms.DataGridViewAutoSizeRowsMode.AllCells;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(12, 102);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.ReadOnly = true;
+            this.dataGridView1.Size = new System.Drawing.Size(776, 291);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(20, 43);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(57, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "From date:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(30, 69);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(47, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "To date:";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // dtStartDate
+            // 
+            this.dtStartDate.Format = System.Windows.Forms.DateTimePickerFormat.Short;
+            this.dtStartDate.Location = new System.Drawing.Point(83, 37);
+            this.dtStartDate.Name = "dtStartDate";
+            this.dtStartDate.Size = new System.Drawing.Size(119, 20);
+            this.dtStartDate.TabIndex = 3;
+            // 
+            // dtEndDate
+            // 
+            this.dtEndDate.Format = System.Windows.Forms.DateTimePickerFormat.Short;
+            this.dtEndDate.Location = new System.Drawing.Point(83, 63);
+            this.dtEndDate.Name = "dtEndDate";
+            this.dtEndDate.Size = new System.Drawing.Size(119, 20);
+            this.dtEndDate.TabIndex = 4;
+            // 
+            // btnLoad
+            // 
+            this.btnLoad.Location = new System.Drawing.Point(208, 38);
+            this.btnLoad.Name = "btnLoad";
+            this.btnLoad.Size = new System.Drawing.Size(147, 23);
+            this.btnLoad.TabIndex = 5;
+            this.btnLoad.Text = "Search by trackingDate";
+            this.btnLoad.UseVisualStyleBackColor = true;
+            this.btnLoad.Click += new System.EventHandler(this.btnLoad_Click);
+            // 
+            // textBoxTrackingCode
+            // 
+            this.textBoxTrackingCode.Location = new System.Drawing.Point(410, 40);
+            this.textBoxTrackingCode.Name = "textBoxTrackingCode";
+            this.textBoxTrackingCode.Size = new System.Drawing.Size(100, 20);
+            this.textBoxTrackingCode.TabIndex = 6;
+            // 
+            // btnSearch
+            // 
+            this.btnSearch.Location = new System.Drawing.Point(516, 40);
+            this.btnSearch.Name = "btnSearch";
+            this.btnSearch.Size = new System.Drawing.Size(155, 23);
+            this.btnSearch.TabIndex = 7;
+            this.btnSearch.Text = "Search by trackingCode";
+            this.btnSearch.UseVisualStyleBackColor = true;
+            this.btnSearch.Click += new System.EventHandler(this.btnSearch_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 405);
+            this.Controls.Add(this.btnSearch);
+            this.Controls.Add(this.textBoxTrackingCode);
+            this.Controls.Add(this.btnLoad);
+            this.Controls.Add(this.dtEndDate);
+            this.Controls.Add(this.dtStartDate);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.dataGridView1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.DateTimePicker dtStartDate;
+        private System.Windows.Forms.DateTimePicker dtEndDate;
+        private System.Windows.Forms.Button btnLoad;
+        private System.Windows.Forms.TextBox textBoxTrackingCode;
+        private System.Windows.Forms.Button btnSearch;
+    }
+}
+
---
+++ HT/HT/Form1.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+
+namespace HT
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnLoad_Click(object sender, EventArgs e)
+        {
+            DateTime startDate = dtStartDate.Value;
+            DateTime endDate = dtEndDate.Value.AddDays(1).Date;
+
+
+            // Construct the SQL query with a parameter for the start and end dates
+            string query = "SELECT s.id, s.trackingCode, s.carrier,  " +
+                "p2.name as receiverName, p2.email as receiverEmail, a.street, a.city, a.zipCode, a.country, " +
+                "s.trackingDate, s.status, s.estimatedDeliveryDate " +
+                "FROM Shipment s " +
+                "JOIN Party p ON s.senderId = p.id " +
+                "JOIN Party p2 ON s.receiverId = p2.id " +
+                "JOIN Address a ON s.addressFromId = a.id " +
+                "WHERE s.trackingDate BETWEEN @startDate AND @endDate";
+
+            // Create a SqlCommand object with the query and parameters
+            string connectionString = "Data Source=(local);Initial Catalog=ShipmentTrackingDB;Integrated Security=True";
+            SqlConnection connection = new SqlConnection(connectionString);
+            using (SqlCommand command = new SqlCommand(query, connection))
+            {
+                command.Parameters.AddWithValue("@startDate", startDate);
+                command.Parameters.AddWithValue("@endDate", endDate);
+
+                // Create a SqlDataAdapter to execute the command and fill a DataTable
+                SqlDataAdapter adapter = new SqlDataAdapter(command);
+                DataTable dataTable = new DataTable();
+                adapter.Fill(dataTable);
+
+                // Set the DataGridView's DataSource property to the DataTable
+                dataGridView1.DataSource = dataTable;
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+ 
+            string connectionString = "Data Source=(local);Initial Catalog=ShipmentTrackingDB;Integrated Security=True";
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                connection.Open();
+
+                SqlCommand command = new SqlCommand("SELECT s.id, s.trackingCode, s.carrier, s.trackingDate, s.status, s.estimatedDeliveryDate, " +
+                    "p2.name as receiverName, p2.email as receiverEmail, p2.phone as receiverPhone, " +
+                    "a.street, a.city, a.zipCode, a.country " +
+                    "FROM Shipment s " +
+                    "JOIN Party p2 ON s.receiverId = p2.id " +
+                    "JOIN Address a ON s.addressFromId = a.id;", connection);
+
+
+                SqlDataAdapter adapter = new SqlDataAdapter(command);
+
+                DataTable dataTable = new DataTable();
+                adapter.Fill(dataTable);
+
+                dataGridView1.DataSource = dataTable;
+            }
+
+
+        }
+
+        private void btnSearch_Click(object sender, EventArgs e)
+        {
+            string searchValue = textBoxTrackingCode.Text.Trim();
+            string sqlQuery = "SELECT s.id, s.trackingCode, s.carrier,  " +
+                "p2.name as receiverName, p2.email as receiverEmail, p2.phone as receiverPhone, " +
+                "a.street, a.city, a.zipCode, a.country, s.trackingDate, s.status, s.estimatedDeliveryDate " +
+                "FROM Shipment s " +
+                "JOIN Party p ON s.senderId = p.id " +
+                "JOIN Party p2 ON s.receiverId = p2.id " +
+                "JOIN Address a ON s.addressFromId = a.id ";
+
+            if (!string.IsNullOrEmpty(searchValue))
+            {
+                sqlQuery += "WHERE s.trackingCode LIKE @TrackingCode";
+            }
+
+            string connectionString = "Data Source=(local);Initial Catalog=ShipmentTrackingDB;Integrated Security=True";
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                SqlCommand command = new SqlCommand(sqlQuery, connection);
+                if (!string.IsNullOrEmpty(searchValue))
+                {
+                    command.Parameters.AddWithValue("@TrackingCode", "%" + searchValue + "%");
+                }
+                SqlDataAdapter dataAdapter = new SqlDataAdapter(command);
+                DataTable dataTable = new DataTable();
+                dataAdapter.Fill(dataTable);
+                dataGridView1.DataSource = dataTable;
+            }
+
+        }
+    }
+}
